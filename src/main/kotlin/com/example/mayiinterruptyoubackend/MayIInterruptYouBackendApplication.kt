2991842f23673b2a93754844cd03b6a6d7b04242
rubyfeller <ruby.feller@student.fontys.nl>
2023-04-21 11:10:24 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/mayiinterruptyoubackend/MayIInterruptYouBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mayiinterruptyoubackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MayIInterruptYouBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MayIInterruptYouBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/mayiinterruptyoubackend/MayIInterruptYouBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mayiinterruptyoubackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MayIInterruptYouBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
